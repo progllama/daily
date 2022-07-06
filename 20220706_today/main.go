f84fafa12ac2eb781af4c46448ccba6859f2d1b5
Yuta Tachihara <>
2022-07-07 02:45:19 +0900

    WIP

--- 20220706_today/main.go
+++ 20220706_today/main.go
@@ -17,18 +17,49 @@ func main() {
 
 	args := os.Args[1:]
 	for _, arg := range args {
-		res, err := http.Get(arg)
+		url := arg
+		if arg[:8] != "https://" && arg[:7] != "http://" {
+			url = "https://" + url
+		}
+
+		res, err := http.Get(url)
 		if err != nil {
 			logger.Println(err)
 			continue
 		}
+
 		if res.StatusCode != http.StatusOK {
 			logger.Println(res.StatusCode)
 			continue
 		}
+
 		scanner := bufio.NewScanner(res.Body)
+		var body string
 		for scanner.Scan() {
-			logger.Println(scanner.Text())
+			body += scanner.Text()
+		}
+
+		currentNode := Node{}
+		tag := false
+		for i, c := range body {
+			if string(c) == "<" {
+				tag = true
+			}
+
+			if string(c) == ">" {
+				tag = false
+			}
+
+			if tag {
+
+			} else {
+
+			}
 		}
 	}
+}
+
+type Node struct {
+	ParentNode *Node
+	ChildNodes []Node
 }
